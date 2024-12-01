6e59bb455e0b8843bbb714c87f122eebd5520221
williamalowe <williamlowedev@gmail.com>
2024-12-02 09:43:52 +1100

    styles: add newDockitModal mobile view

--- src/components/NewDockittModal.tsx
+++ src/components/NewDockittModal.tsx
@@ -3,7 +3,7 @@ import NewDockittForm from "./NewDockettForm";
 const NewDockittModal = () => {
   return (
     <>
-      <label htmlFor="my_modal_6" className="fixed bottom-4 right-[calc(50%-14rem)] btn btn-primary w-full max-w-md">
+      <label htmlFor="my_modal_6" className="fixed bottom-2 btn btn-primary w-[80vw] max-w-md">
         New Dockitt
       </label>
 
