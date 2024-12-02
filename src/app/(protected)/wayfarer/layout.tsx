4161cb282d19575c536d3b14aa6fc05a21f693b8
williamalowe <williamlowedev@gmail.com>
2024-12-02 17:29:14 +1100

    fix: resolve wayfarer sidebar values

--- src/app/(protected)/wayfarer/layout.tsx
+++ src/app/(protected)/wayfarer/layout.tsx
@@ -9,7 +9,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
     <div className="min-h-screen flex flex-col px-2 lg:px-8 py-4">
       <div className='flex items-center'>
         <div className='flex items-center'>
-          <Sidebar selectedProject='Wayfarer'/>
+          <Sidebar selectedProject='wayfarer'/>
           <ProjectDropdown />
         </div>
         <h1 className='flex-1 text-center font-bold text-2xl'>Wayfarer Project Board</h1>
