7260ec8a55b0a0a22458aa1d6b5f622e646d0914
williamalowe <williamlowedev@gmail.com>
2024-12-02 16:46:30 +1100

    styles: various styling chages

--- src/components/ProjectDropdown.tsx
+++ src/components/ProjectDropdown.tsx
@@ -28,12 +28,6 @@ const ProjectDropdown = () => {
             <Link href={project.href}>{project.title}</Link>
           </li>
         ))}
-        {/* <li>
-          <a>Item 1</a>
-        </li>
-        <li>
-          <a>Item 2</a>
-        </li> */}
       </ul>
     </div>
   );
